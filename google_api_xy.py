4af9017322c19a773588927e65af8b252be9a5f1
Jocelyn Chen <40486653+jolenechen123@users.noreply.github.com>
2018-11-25 20:18:14 +0800

    Create google_api_xy.py
    文字辨識code

---
+++ google_api_xy.py
@@ -0,0 +1,56 @@
+import numpy as np
+import io
+import os
+import string
+import cv2
+ans = []
+def detect_text(path):
+
+ 
+    
+
+   
+    img = cv2.imread("test.jpg")
+  
+    x = 0
+    y = 0
+
+    w = 600
+    h = 60
+
+  
+    crop_img = img[y:y+h, x:x+w]
+   
+
+    cv2.imwrite('crop.jpg', crop_img)
+    
+    
+    from google.cloud import vision
+    from google.cloud.vision import types
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/lampp/htdocs/upload/ainurse-ddf01edb5c56.json"
+    client = vision.ImageAnnotatorClient()
+    
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+
+   
+    for text in texts:
+
+        an = text.description
+        ans.append(an)
+    for i in range(1,6):
+        ans[i]= ans[i].replace("(","")
+        ans[i]= ans[i].replace(")","")
+    return (ans[2])
+
+
+
+print(detect_text("/opt/lampp/htdocs/upload/crop.jpg"))
